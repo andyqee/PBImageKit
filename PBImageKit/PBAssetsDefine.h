e6a1d38a3e04996d2cfdfb651042eebd5258d8d4
andyqee <vitasone@gmail.com>
2015-05-11 14:56:12 +0800

    add pod spec

--- PBImageKit/PBAssetsDefine.h
+++ PBImageKit/PBAssetsDefine.h
@@ -22,6 +22,12 @@ typedef NS_ENUM(NSUInteger, PBAssetImageResolution) {
     kPBAssetImageResolutionFullsize
 };
 
+//定义屏幕高度
+#define ScreenHeight [UIScreen mainScreen].bounds.size.height
+//定义屏幕宽度
+#define ScreenWidth [UIScreen mainScreen].bounds.size.width
+//定义颜色
+#define Color(r, g, b, a) [UIColor colorWithRed:r/255.0 green:g/255.0 blue:b/255.0 alpha:a]
 
 #endif
 
--- PBImageKit/PBImageKit.h
+++ PBImageKit/PBImageKit.h
@@ -16,4 +16,6 @@ FOUNDATION_EXPORT const unsigned char PBImageKitVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <PBImageKit/PublicHeader.h>
 
+#import "PBImageManager.h"
+
 
